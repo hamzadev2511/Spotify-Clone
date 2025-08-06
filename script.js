ccb8e7c92f33c6c930ccb066d73d58008de5b2db
hamzadev2511 <hamzaiqbal2511@gmail.com>
2025-08-06 18:43:52 +0500

    add all these channges

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let currentSong = new Audio()  
+
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+
 async function getSongs() {
     let a = await fetch("http://127.0.0.1:3000/songs/");
     let response = await a.text()
@@ -9,26 +26,61 @@ async function getSongs() {
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href)
+            songs.push(element.href.split("/songs/")[1])
         }
     }
     return songs;
-    
+} 
+const playMusic = (track)=>{
+    currentSong.src = "/songs/" + track
+    currentSong.play();
+    play.src = "pause.svg"
+    document.querySelector(".songinfo").innerHTML = track;
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"; 
 }
+
+
 async function main(){
+
     //Get All The Songs
 let songs = await getSongs ();
-console.log(songs); 
 
-    //Play The first song
-  var audio = new Audio(songs[1]);
-  audio.play();   
-  audio.addEventListener("loadeddata", () => {
-  let duration = audio.duration;
-  console.log(duration);
-  
-  // The duration variable now holds the duration (in seconds) of the audio clip
-});
-}      
+  let songUL =  document.querySelector(".songList").getElementsByTagName("ul")[0]
+  for (const song of songs) {
+    songUL.innerHTML = songUL.innerHTML + `<li> <img class="invert" src="music.svg" alt="">
+                            <div class="info">
+                                <div> ${song.replaceAll("%20", " ")}</div>
+                                <div>Hamza</div>
+                            </div>
+                            <div class="playnow">
+                            <span>Play Now</span>
+                            <img class="invert" src="play.svg" alt="">
+                            </div></li> `; 
+  } 
+  Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+    e.addEventListener("click", element=>{
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)    
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+    })
+
+  });
+
+  play.addEventListener("click", ()=>{
+    if(currentSong.paused){
+        currentSong.play()
+        play.src = "pause.svg"
+    }
+    else{
+        currentSong.pause()
+        play.src = "play.svg"
+    }
+  })
+   currentSong.addEventListener("timeupdate", () => {
+        console.log(currentSong.currentTime, currentSong.duration)
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`
+        document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
+    })
+
+}    
 
 main();
